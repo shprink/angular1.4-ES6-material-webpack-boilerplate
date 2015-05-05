0009d8f3e2e7e33c7810d444672e41529c84a164
shprink <julien.renaux@gmail.com>
2015-05-04 20:59:34 -0500

    packaging

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # angular1.4-ES6-material-webpack-boilerplate
 A simple AngularJS 1.4 boilerplate using ES6, material design and webpack
+
+## Install
+
+```
+npm install
+```
+
+## Dev (liverelaod server)
+
+```
+npm run-script devserver
+```
+
+## Build
+
+```
+npm run-script build
+```
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path'),
+    webpack = require("webpack"),
+    libPath = path.join(__dirname, 'lib'),
+    wwwPath = path.join(__dirname, 'www'),
+    pkg = require('./package.json'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
+
+module.exports = {
+    entry: path.join(libPath, 'index.js'),
+    output: {
+        path: path.join(wwwPath),
+        filename: 'bundle-[hash:6].js'
+    },
+    module: {
+        loaders: [{
+            test: /\.html$/,
+            loader: 'file?name=templates/[name]-[hash:6].html'
+        }, {
+            test: /\.(png|jpg)$/,
+            loader: 'file?name=img/[name].[ext]' // inline base64 URLs for <=10kb images, direct URLs for the rest
+        }, {
+            test: /\.css$/,
+            loader: "style!css"
+        }, {
+            test: /\.js$/,
+            loader: "babel"
+        }, {
+            test: [/fontawesome-webfont\.svg/, /fontawesome-webfont\.eot/, /fontawesome-webfont\.ttf/, /fontawesome-webfont\.woff/, /fontawesome-webfont\.woff2/],
+            loader: 'file?name=fonts/[name].[ext]'
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            pkg: pkg,
+            template: path.join(libPath, 'index.html')
+        }),
+        new ngAnnotatePlugin({
+            add: true
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+    ]
+};
